3ec5531c9ac1e55076c68d6623b64a96e9c5516d
7thStringofZhef <68602513+DavidSlayback@users.noreply.github.com>
2022-03-23 14:21:49 -0400

    Push version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="po_brax",
-    version="0.0.1",
+    version="0.0.11",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Development Status :: 3 - Alpha",
@@ -12,6 +12,6 @@ setup(
         "Operating System :: MacOS :: MacOS X",
     ],
     packages=[pkg for pkg in find_packages() if pkg.startswith('po_brax')],
-    install_requires=["gym>=0.21.0", "numpy<1.22,>=1.21", "numba>=0.55.1", "brax>=0.0.10"],
+    install_requires=["gym>=0.21.0", "numpy<1.22,>=1.21", "numba>=0.55.1", "brax>=0.0.12"],
     python_requires=">=3.8",
 )
